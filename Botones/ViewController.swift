6c65d075f3d8505ca93e48e2d6cfb8ca62feea19
davidSospina <david.12.ospina@gmail.com>
2020-06-30 09:28:28 -0500

    Switch, Page Controls, Sliders, Steppers, Progress Indicators, Text Field, Text View

--- Botones/ViewController.swift
+++ Botones/ViewController.swift
@@ -15,6 +15,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var myPickerView: UIPickerView!
     @IBOutlet weak var myPageControl: UIPageControl!
     @IBOutlet weak var mySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var mySlider: UISlider!
+    @IBOutlet weak var myStepper: UIStepper!
+    @IBOutlet weak var mySwitch: UISwitch!
+    @IBOutlet weak var myActivityView: UIActivityIndicatorView!
+    @IBOutlet weak var myProgressView: UIProgressView!
+    @IBOutlet weak var myStepperLabel: UILabel!
+    @IBOutlet weak var mySwitchLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myTextValue: UITextView!
+    @IBOutlet weak var myTextView: UITextView!
     
     //Variables
     
@@ -33,6 +43,7 @@ class ViewController: UIViewController {
         myPickerView.backgroundColor = .lightGray
         myPickerView.dataSource = self
         myPickerView.delegate = self
+        myPickerView.isHidden = true
         
         // PageControls
         myPageControl.numberOfPages = myPickerViewValues.count
@@ -46,6 +57,48 @@ class ViewController: UIViewController {
         for(index, value) in myPickerViewValues.enumerated(){
             mySegmentedControl.insertSegment(withTitle: value, at: index, animated: true)
         }
+        
+        //Sliders
+        
+        mySlider.minimumTrackTintColor = .red
+        //mySlider.maximumTrackTintColor = .blue
+        mySlider.minimumValue = 0
+        mySlider.maximumValue = Float(myPickerViewValues.count)
+        
+        //Valor por defecto
+        mySlider.value = 0
+        
+        //Stepper
+        myStepper.minimumValue = 0
+        myStepper.maximumValue = Double(myPickerViewValues.count)
+        
+        //Switches
+        mySwitch.onTintColor = .blue
+        mySwitch.isOn = false
+        
+        //Progress
+        myProgressView.progress = 0
+        
+        myActivityView.color = .orange
+        myActivityView.startAnimating()
+        myActivityView.hidesWhenStopped = true
+        
+        // Labels
+        myStepperLabel.textColor = .brown
+        myStepperLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        myStepperLabel.text = "0"
+        
+        mySwitchLabel.text = "Apagado"
+        
+        // TextField
+        myTextField.textColor = .brown
+        myTextField.placeholder = "Hola mundo"
+        myTextField.delegate = self
+        
+        // TextView
+        myTextValue.textColor = .brown
+        myTextValue.delegate = self
     }
 
     //Actions
@@ -55,6 +108,8 @@ class ViewController: UIViewController {
         } else {
             myButton.backgroundColor = .blue
         }
+        
+        myTextValue.resignFirstResponder()
     }
     
     @IBAction func myPageCOntrolAction(_ sender: Any) {
@@ -64,6 +119,11 @@ class ViewController: UIViewController {
         myButton.setTitle(myString, for: .normal)
         
         mySegmentedControl.selectedSegmentIndex = myPageControl.currentPage
+        
+        mySlider.value = Float(myPageControl.currentPage)
+        
+        myStepper.value =
+            Double(myPageControl.currentPage)
     }
     
     @IBAction func mySegmentedControlAction(_ sender: Any) {
@@ -73,8 +133,120 @@ class ViewController: UIViewController {
         myButton.setTitle(myString, for: .normal)
         
         myPageControl.currentPage = mySegmentedControl.selectedSegmentIndex
+        
+        mySlider.value = Float(mySegmentedControl.selectedSegmentIndex)
+        
+        myStepper.value =
+            Double(mySegmentedControl.selectedSegmentIndex)
     }
     
+    @IBAction func mySliderAction(_ sender: Any) {
+        
+        
+        var myString = ""
+        
+        myPickerView.selectRow(mySegmentedControl.selectedSegmentIndex, inComponent: 0, animated: true)
+
+        var progress:Float = 0.0
+        
+        switch mySlider.value {
+        case 0..<1:
+            myString = myPickerViewValues[0]
+            
+            progress = 0.0
+            
+            myButton.setTitle(myString, for: .normal)
+            
+            mySegmentedControl.selectedSegmentIndex = 0
+            mySlider.value = 0
+            myStepper.value = 0
+        case 1..<2:
+            myString = myPickerViewValues[1]
+            progress = 0.2
+            
+            myButton.setTitle(myString, for: .normal)
+            
+            mySegmentedControl.selectedSegmentIndex = 1
+            mySlider.value = 1
+            myStepper.value = 1
+        case 2..<3:
+            myString = myPickerViewValues[2]
+            progress = 0.4
+            
+            myButton.setTitle(myString, for: .normal)
+            
+            mySegmentedControl.selectedSegmentIndex = 2
+            mySlider.value = 2
+            myStepper.value = 2
+        case 3..<4:
+            myString = myPickerViewValues[3]
+            progress = 0.6
+            
+            myButton.setTitle(myString, for: .normal)
+            
+            mySegmentedControl.selectedSegmentIndex = 3
+            mySlider.value = 3
+            myStepper.value = 3
+        case 4..<5:
+            myString = myPickerViewValues[4]
+            progress = 0.8
+            
+            myButton.setTitle(myString, for: .normal)
+            
+            mySegmentedControl.selectedSegmentIndex = 4
+            mySlider.value = 4
+            myStepper.value = 4
+        case 5..<6:
+            myString = myPickerViewValues[5]
+            progress = 1
+            
+            myButton.setTitle(myString, for: .normal)
+            
+            mySegmentedControl.selectedSegmentIndex = 5
+            mySlider.value = 5
+            myStepper.value = 5
+        default:
+            myString = myPickerViewValues[0]
+            progress = 0.0
+            
+            myButton.setTitle(myString, for: .normal)
+            
+            mySegmentedControl.selectedSegmentIndex = 0
+            mySlider.value = 0
+            myStepper.value = 0
+        }
+        
+        myProgressView.progress = progress
+    }
+    @IBAction func myStepperAction(_ sender: Any) {
+        let valueStepper = myStepper.value
+        
+        let myString = myPickerViewValues[mySegmentedControl.selectedSegmentIndex]
+        
+        myPickerView.selectRow(Int(myStepper.value), inComponent: 0, animated: true)
+        
+        mySlider.value = Float(valueStepper)
+        
+        myButton.setTitle(myString, for: .normal)
+        
+        myPageControl.currentPage = Int(valueStepper)
+        
+        mySegmentedControl.selectedSegmentIndex = Int(valueStepper)
+        
+        myStepperLabel.text = "\(valueStepper)"
+    }
+    
+    @IBAction func mySwitchAction(_ sender: Any) {
+        if mySwitch.isOn {
+            myPickerView.isHidden = false
+            myActivityView.stopAnimating()
+            mySwitchLabel.text = "Encendido"
+        } else{
+            myPickerView.isHidden = true
+            myActivityView.startAnimating()
+            mySwitchLabel.text = "Apagado"
+        }
+    }
 }
 
 // Extensión para no agragar más datos al ViewController
@@ -104,7 +276,31 @@ UIPickerViewDelegate {
         myPageControl.currentPage = row
         
         mySegmentedControl.selectedSegmentIndex = row
+        
+        mySlider.value = Float(row)
+        
+        myStepper.value = Double(row)
     }
     
+}
+
+extension ViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        myButton.setTitle(myTextField.text, for: .normal)
+    }
+}
+
+extension ViewController: UITextViewDelegate{
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        myTextField.isHidden = true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        myTextField.isHidden = false
+    }
 }
 
